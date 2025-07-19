51051b6bbc1f23987bf347e0714227abe51ed9b3
sistemvene <motasistem@gmail.com>
2025-07-19 20:53:40 +0200

    Actualización de frontend: index.html, vite.config.js, package.json

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
+  root: process.cwd(),
+  base: './',
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
   },
 });
