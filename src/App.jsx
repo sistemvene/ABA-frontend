ca901276a283dfe7a17fb46239dd87d0957860de
root <root@backend.dwfz4uc1ohmeppd22ncvqq525g.zrhx.internal.cloudapp.net>
2025-07-19 17:46:45 +0000

    Initial commit for frontend

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ExampleComponent from './components/ExampleComponent';
+
+export default function App() {
+  return (
+    <div className="app-container">
+      <h1>Panel de Administración</h1>
+      <ExampleComponent />
+    </div>
+  );
+}
